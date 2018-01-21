b4388068851a56c1eb6673b69267f4f46d38f23f
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2018-01-20 18:48:45 -0600

    added requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,8 +138,6 @@ STATICFILES_DIRS = (
 )
 
 
-STATIC_URL = '/static/'
-
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
