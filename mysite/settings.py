8b4d260a1ee94013b66e62f68f82463e6aac6382
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2018-01-21 20:47:19 -0600

    added runtime

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,9 +138,11 @@ STATICFILES_DIRS = (
 )
 
 
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
